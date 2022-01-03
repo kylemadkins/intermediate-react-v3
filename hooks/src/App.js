ca2cd8d54227fa97db08752515abd4fff59f3593
Kyle Adkins <kyle.m.adkins@gmail.com>
2022-01-02 22:38:19 -0600

    Create app and add useState example

---
+++ hooks/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import State from './State';
+
+function App() {
+  return (
+    <div className="App">
+      <State />
+      <hr />
+    </div>
+  );
+}
+
+export default App;
