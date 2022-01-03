944c72544d55ec81463c59a51887b3b198418b81
Kyle Adkins <kyle.m.adkins@gmail.com>
2022-01-02 23:02:27 -0600

    Add useEffect example

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import State from './State';
+import Effect from './Effect';
 
 function App() {
   return (
     <div className="App">
       <State />
+      <hr />
+      <Effect />
       <hr />
     </div>
   );
